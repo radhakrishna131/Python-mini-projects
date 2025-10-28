b965e8b6ec69fad28aa162d182000f6bf037ec52
radhakrishna131 <radhakrishna628191@gmail.com>
2025-10-28 21:29:39 +0530

    Simple calculator
    Refactor calculator to use functions for operations

--- simple calculator.py
+++ simple calculator.py
@@ -1,34 +1,48 @@
-while True:
-    num1=float(input("Enter first number:"))
-    num2=float(input("Enter second number:"))
-    print("1.addition")
-    print("2.subtraction")
-    print("3.multiplication")
-    print("4.division")
-    print("5.power")
-	
-    operator=input("choose the operator:")
+ def sum(m, n):
+    return f"the sum of the numbers is {m+n}"
 
-    if operator=="1":
-    	print(f"addition of this two numbers is {num1+num2}")
-	
-    elif operator=="2":
-    	print(f"subtraction of this two numbers is {num1-num2}")
-	
-    elif operator=="3":
-    			print(f"multiplication of this two numbers is {num1*num2}")
-			
-    elif operator=="4":
-		    	print(f"division of this two numbers is {num1/num2}")
-			
-    elif operator=="5":
-    	print(f"power of this two numbers is {num1**num2}")
-	
+
+def sub(m, n):
+    return f"the subtraction of the numbers is {m-n}"
+
+
+def multi(m, n):
+    return f"the multiplication of the numbers is {m*n}"
+
+
+def div(m, n):
+    return f"the division of the numbers is {m/n}"
+
+
+def expo(m, n):
+    return f"the power of the numbers is {m**n}"
+
+
+def fact(m):
+    if m == 0:
+        return 1
     else:
-    	print("invalid operator")	
-	
-							
-													
-																			
-																									
-    print("[Thank you for using this calculator]")																	
+        return m * fact(m - 1)
+
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+opp = input("Choose the operator(+,-,*,/,**,!):")
+
+if opp == "+":
+    print(sum(num1, num2))
+elif opp == "-":
+    print(sub(num1, num2))
+elif opp == "*":
+    print(multi(num1, num2))
+elif opp == "/":
+    print(div(num1, num2))
+elif opp == "**":
+    print(expo(num1, num2))
+elif opp == "!":
+    print(
+        f"the factorial of fist number is {fact(num1)}\nthe factorial of second number is {fact(num2)}"
+    )
+else:
+    print("please choose the correct opperator")
+																	
